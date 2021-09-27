c3b28d0a5f8d861ee3a834ce67f29474b49be8f3
Voneska <91376307+Voneska@users.noreply.github.com>
2021-09-27 18:17:17 +0300

    Изменение имени объекта (того же что и в ветке check2) в ветке check1, перешедшего из основной ветки

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -26,8 +26,8 @@ public:
 
 int main()
 {
-	Figures BlackHorse(1), WhiteHorse;
-	std::cout << BlackHorse.checkcolor() << "\t" << WhiteHorse.checkcolor()<< std::endl;
+	Figures BlackHorseHorse, WhiteHorseHorse;
+	std::cout << BlackHorseHorse.checkcolor() << "\t" << WhiteHorseHorse.checkcolor()<< std::endl;
 
 
 	HPEN pen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255));

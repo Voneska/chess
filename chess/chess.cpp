63bc21e6a27746872300156a324f5a2b6d9d03b6
Arey <pdasam33@gmail.com>
2021-09-27 17:10:09 +0300

    Resp1

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -9,6 +9,14 @@ int main()
 
 	printf("hello word");
 	std::cout << "\nHello word2\n";
+	HPEN pen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255));
+	HBRUSH pen1 = CreateSolidBrush(RGB(137, 104, 37));
+	SelectObject(hdc, pen);
+	SelectObject(hdc, pen1);
+	Rectangle(hdc, 60, 60, 200, 200);
+	DeleteObject(pen);
+	DeleteObject(pen1);
+	getchar();
 	return 0;
 }
 

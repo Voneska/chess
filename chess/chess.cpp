bb52d170234d3fd4fe955452df8e217c2ce1816a
Voneska <91376307+Voneska@users.noreply.github.com>
2021-10-16 12:37:56 +0300

    Menu

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -1,22 +1,10 @@
-﻿/* 
-Кодировка
-_                      _ 
-2 - пустая ячейка      3 - пешка
-0 - черная фигура      4 - конь
-1 - белая фигура	   ...
-
-Например:
-03 - черная пешка
-*/
-
-
-#include <iostream>
+﻿#include <iostream>
 #include <Windows.h>
 
 HWND hwnd = GetConsoleWindow();
 HDC hdc = GetDC(hwnd);
 //class menu
-class Menu{
+class Menu {
 private:
 	int user_choise = 0;
 
@@ -29,8 +17,11 @@ private:
 		std::cout << "Your choise: ";
 	}
 
+public:
 	void New_game() {
 		system("cls");
+		std::cout << "New game have been starting!" << std::endl;
+		Sleep(1000);
 	}
 	void Reference() {
 		system("cls");
@@ -51,7 +42,6 @@ private:
 		Sleep(500);
 	}
 
-public:
 	int set_choise() {
 		menu_window();
 		std::cin >> user_choise;
@@ -59,98 +49,33 @@ public:
 			std::cin.clear(); // clears error flags
 			std::cin.ignore(2323, '\n'); // ~ while(cin.get() != '\n');
 		}
-		switch (user_choise) {
-		case 1: 
-			New_game();
-			return user_choise;
-			break;
-		case 2: 
-			Reference();
-			return user_choise;
-			break;
-		case 3: Exit();
-			return user_choise;
-			break;
-		default:
-			Err_choise();
-			return 4;
-		}
+		return user_choise;
 	}
 	int give_choise() { return user_choise; }
 };
 
 
-//function menu
-/*
-int menu() {
-	int user_choise1 = 0;
-	system("cls");
-	std::cout << "\tHello, stranger." << std::endl;
-	std::cout << "1. New game" << std::endl;
-	std::cout << "2. Reference" << std::endl;
-	std::cout << "3. Exit" << std::endl;
-	std::cout << "Your choise: ";
-	std::cin >> user_choise1;
-	if (std::cin.fail()) { // check error flags
-		std::cin.clear(); // clears error flags
-		std::cin.ignore(2323, '\n'); // ~ while(cin.get() != '\n');
-	}
-	switch (user_choise1) {
-	case 1:
-		system("cls");
-		return user_choise1;
-		break;
-	case 2:		
-		system("cls");
-		std::cout << "Write:" << std::endl;
-		std::cout << "1. \"Pass\" to surrender." << std::endl;
-		std::cout << "2. \"Draw\" to offer a draw." << std::endl;
-		std::cout << "Type enter to exit reference.";
-		std::cin.get();
-		std::cin.get();
-		return user_choise1;
-		break;
-	case 3: 
-		system("cls");
-		std::cout << "Thanks for playing. Good bye." << std::endl;
-		return user_choise1;
-		break;
-	default:
-		system("cls");
-		std::cout << "It's wrong!" << std::endl;
-		Sleep(500);
-		return 4;
-	}
-}
-*/
-
 int main()
 {
-	Menu game;
-	int a =0;
-	while ((a = game.set_choise()) != 3){
-		if (a == 4) continue;
-		if (a == 2) continue;
-		//if (a == 1) {
+	Menu game_menu;
 
-		std::cout << "Hey" << std::endl;
-		Sleep(1000);
-		//}
-
+	switch (game_menu.set_choise()) {
+	case 1:
+		game_menu.New_game();
+		main();
+		break;
+	case 2:
+		game_menu.Reference();
+		main();
+		break;
+	case 3:
+		game_menu.Exit();
+		break;
+	default:
+		game_menu.Err_choise();
+		main();
 	}
 
-	//for Function
-	/*
-	int a = 0;
-	while ((a = menu()) != 3) {
-		if (a == 4) continue;
-		if (a == 2) continue;
-		//if (a == 1) {
 
-		std::cout << "Hey" << std::endl;
-		Sleep(1000);
-		//}
-	}*/
-	
 	return 0;
 }

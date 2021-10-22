eff5fb8e2d0b8d14c2959b845c120078811e91d3
Arey <pdasam33@gmail.com>
2021-10-22 21:54:35 +0300

    chek_cell+choose_color

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -5,11 +5,45 @@ using namespace std;
 string white, black;
 unsigned short cell[8][8];
 bool turn = 0;//xod belix
+
+void translatePosition(string position, unsigned short* pos)
+{
+	switch (position.at(0))
+	{
+	case 'A':
+	case 'a': pos[0] = 0;
+		break;
+	case 'B':
+	case 'b': pos[0] = 1;
+		break;
+	case 'C':
+	case 'c': pos[0] = 2;
+		break;
+	case 'D':
+	case 'd': pos[0] = 3;
+		break;
+	case 'E':
+	case 'e': pos[0] = 4;
+		break;
+	case 'F':
+	case 'f': pos[0] = 5;
+		break;
+	case 'G':
+	case 'g': pos[0] = 6;
+		break;
+	case 'H':
+	case 'h': pos[0] = 7;
+		break;
+	}
+	pos[1] = position.at(1) - '0' - 1;
+}
+
 void gen_color()
 {
 	srand(time(0));
 	if (rand() % 2 == 1) swap(white, black);
 }
+
 void choose_player()
 {
 	string choose, choose_c1, choose_c2;
@@ -62,58 +96,114 @@ void victory()
 		cout << "Checkmate to the black King\n" << white << " wooooooon!!!\n";
 	else if (turn == 1) cout << "Checkmate to the white King\n" << black << " wooooooon!!!\n";
 }
-//void start_cell()
-//{
-//	cell[0][0] = 14; cell[0][1] = 12; cell[0][2] = 13; cell[0][3] = 15;
-//	cell[0][4] = 16; cell[0][5] = 13; cell[0][6] = 12; cell[0][7] = 14;
-//	for (int i = 0; i < 8; i++)
-//	{
-//		cell[1][i] = 11;
-//		cell[6][i] = 21;
-//	}
-//	cell[7][0] = 24; cell[7][1] = 22; cell[7][2] = 23; cell[7][3] = 25;
-//	cell[7][4] = 26; cell[7][5] = 23; cell[7][6] = 22; cell[7][7] = 24;
-//}
-bool chek_cell(unsigned short hod[])//vozvrashaet 0 v slychae esli cletca ne bietsa
-{
-	//krol + peski
-	for (int i = hod[1]; i < 8; i++)//up
-	{
-		unsigned short cvert = hod[0] - 1;
-		if ((cell[i][cvert] / 10) == (turn + 1))  break;
-		if ((cell[i][cvert] / 10) != 0)
-			if (((cell[i][cvert] % 10) == 4) || ((cell[i][cvert] % 10) == 5)) return 1;
-			else break;
-	}
-	for (int i = hod[1] - 2; i >= 0; i--)//down
-	{
-		unsigned short cvert = hod[0] - 1;
-		if ((cell[i][cvert] / 10) == (turn + 1))  break;
-		if ((cell[i][cvert] / 10) != 0)
-			if (((cell[i][cvert] % 10) == 4) || ((cell[i][cvert] % 10) == 5)) return 1;
-			else break;
-	}
-	for (int i = hod[0]; i < 8; i++)//->
-	{
-		unsigned short chor = hod[1] - 1;
-		if ((cell[chor][i] / 10) == (turn + 1))  break;
-		if ((cell[chor][i] / 10) != 0)
-			if (((cell[chor][i] % 10) == 4) || ((cell[chor][i] % 10) == 5)) return 1;
-			else break;
-	}
-	for (int i = hod[0] - 2; i >= 0; i--)//<-
-	{
-		unsigned short chor = hod[1] - 1;
-		if ((cell[chor][i] / 10) == (turn + 1))  break;
-		if ((cell[chor][i] / 10) != 0)
-			if (((cell[chor][i] % 10) == 4) || ((cell[chor][i] % 10) == 5)) return 1;
-			else break;
-	}
 
+void start_cell()
+{
+	cell[0][0] = 14; cell[0][1] = 12; cell[0][2] = 13; cell[0][3] = 15;
+	cell[0][4] = 16; cell[0][5] = 13; cell[0][6] = 12; cell[0][7] = 14;
+	for (int i = 0; i < 8; i++)
+	{
+		cell[1][i] = 11;
+		cell[6][i] = 21;
+	}
+	cell[7][0] = 24; cell[7][1] = 22; cell[7][2] = 23; cell[7][3] = 25;
+	cell[7][4] = 26; cell[7][5] = 23; cell[7][6] = 22; cell[7][7] = 24;
+}
+
+bool chek_cell(string s)//vozvrashaet 0 v slychae esli cletca ne bietsa
+{
+	unsigned short hod[2];
+	translatePosition(s, hod);
+	//krol + peski
+	for (int i = hod[1]+1; i < 8; i++)//n
+	{
+		if ((cell[i][hod[0]] / 10) == (turn + 1))  break;
+		if ((cell[i][hod[0]] / 10) != 0)
+			if (((cell[i][hod[0]] % 10) == 4) || ((cell[i][hod[0]] % 10) == 5)) return 1;
+			else break;
+	}
+	for (int i = hod[1] - 1; i >= 0; i--)//s
+	{
+		if ((cell[i][hod[0]] / 10) == (turn + 1))  break;
+		if ((cell[i][hod[0]] / 10) != 0)
+			if (((cell[i][hod[0]] % 10) == 4) || ((cell[i][hod[0]] % 10) == 5)) return 1;
+			else break;
+	}
+	for (int i = hod[0]+1; i < 8; i++)//e
+	{
+		if ((cell[hod[1]][i] / 10) == (turn + 1))  break;
+		if ((cell[hod[1]][i] / 10) != 0)
+			if (((cell[hod[1]][i] % 10) == 4) || ((cell[hod[1]][i] % 10) == 5)) return 1;
+			else break;
+	}
+	for (int i = hod[0] - 1; i >= 0; i--)//w
+	{
+		if ((cell[hod[1]][i] / 10) == (turn + 1))  break;
+		if ((cell[hod[1]][i] / 10) != 0)
+			if (((cell[hod[1]][i] % 10) == 4) || ((cell[hod[1]][i] % 10) == 5)) return 1;
+			else break;
+	}
+	for (int i = hod[1]+1, int j = hod[0]+1; j < 8, i < 8; j++, i++)//n-e
+	{
+		if ((cell[i][j] / 10) == (turn + 1))  break;
+		if ((cell[i][j] / 10) != 0)
+			if (((cell[i][j] % 10) == 3) || ((cell[hod[1]][i] % 10) == 5)) return 1;
+			else break;
+	}
+	for (int i = hod[1]-1, int j = hod[0]-1; j >= 0, i >= 0; j--, i--)//s-w
+	{
+		if ((cell[i][j] / 10) == (turn + 1))  break;
+		if ((cell[i][j] / 10) != 0)
+			if (((cell[i][j] % 10) == 3) || ((cell[hod[1]][i] % 10) == 5)) return 1;
+			else break;
+	}
+	for (int i = hod[1] - 1, int j = hod[0] + 1; j < 8, i >= 0; j++, i--)//s-e
+	{
+		if ((cell[i][j] / 10) == (turn + 1))  break;
+		if ((cell[i][j] / 10) != 0)
+			if (((cell[i][j] % 10) == 3) || ((cell[hod[1]][i] % 10) == 5)) return 1;
+			else break;
+	}
+	for (int i = hod[1] + 1, int j = hod[0] -1; j >= 0, i < 8; j--, i++)//n-w
+	{
+		if ((cell[i][j] / 10) == (turn + 1))  break;
+		if ((cell[i][j] / 10) != 0)
+			if (((cell[i][j] % 10) == 3) || ((cell[hod[1]][i] % 10) == 5)) return 1;
+			else break;
+	}
+	if ((hod[1] + 2 < 8) && (hod[0] + 1 < 8) && (cell[hod[1]+2][hod[0]+1] % 10 == 2) && (cell[hod[1] + 2][hod[0] + 1] / 10 != turn+1)) return 1;//horse
+	if ((hod[1] + 1 < 8) && (hod[0] + 2 < 8) && (cell[hod[1]+1][hod[0]+2] % 10 == 2) && (cell[hod[1] + 1][hod[0] + 2] / 10 != turn + 1)) return 1;
+	if ((hod[1] + 2 < 8) && (hod[0] - 1 >= 0) && (cell[hod[1]+2][hod[0]-1] % 10 == 2) && (cell[hod[1] + 2][hod[0] - 1] / 10 != turn + 1)) return 1;
+	if ((hod[1] + 1 < 8) && (hod[0] - 2 >= 0) && (cell[hod[1]+1][hod[0]-2] % 10 == 2) && (cell[hod[1] + 1][hod[0] - 2] / 10 != turn + 1)) return 1;
+	if ((hod[1] - 2 >= 0) && (hod[0] + 1 < 8) && (cell[hod[1]-2][hod[0]+1] % 10 == 2) && (cell[hod[1] - 2][hod[0] + 1] / 10 != turn + 1)) return 1;
+	if ((hod[1] - 1 >= 0) && (hod[0] + 2 < 8) && (cell[hod[1]-1][hod[0]+2] % 10 == 2) && (cell[hod[1] - 1][hod[0] + 2] / 10 != turn + 1)) return 1;
+	if ((hod[1] - 2 >= 0) && (hod[0] - 1 >= 0) && (cell[hod[1]-2][hod[0]-1] % 10 == 2) && (cell[hod[1] - 2][hod[0] - 1] / 10 != turn + 1)) return 1;
+	if ((hod[1] - 1 >= 0) && (hod[0] - 2 >= 0) && (cell[hod[1]-1][hod[0]-2] % 10 == 2) && (cell[hod[1] - 1][hod[0] - 2] / 10 != turn + 1)) return 1;
+
+	if ((hod[1] + 1 < 8) && (hod[0] + 1 < 8) && (cell[hod[1]+1][hod[0]+1] % 10 == 6) && (cell[hod[1] + 1][hod[0] + 1] / 10 != turn + 1)) return 1;//king
+	if ((hod[1] + 1 < 8) && (cell[hod[1]+1][hod[0]] % 10 == 6) && (cell[hod[1] + 1][hod[0]] / 10 != turn+1)) return 1;
+	if ((hod[1] + 1 < 8) && (hod[0] - 1 >= 0) && (cell[hod[1]+1][hod[0]-1] % 10 == 6) && (cell[hod[1] + 1][hod[0] - 1] / 10 != turn + 1)) return 1;
+	if ((hod[1] - 1 >= 0) && (cell[hod[1]-1][hod[0]] % 10 == 6) && (cell[hod[1] - 1][hod[0]] / 10 != turn+1)) return 1;
+	if ((hod[1] - 1 >= 0) && (hod[0] + 1 < 8) && (cell[hod[1]-1][hod[0]+1] % 10 == 6) && (cell[hod[1] - 1][hod[0] + 1] / 10 != turn + 1)) return 1;
+	if ((hod[0] + 1 < 8) && (cell[hod[1]][hod[0]+1] % 10 == 6) && (cell[hod[1]][hod[0] + 1] / 10 != turn+1)) return 1;
+	if ((hod[1] - 1 >= 0) && (hod[0] - 1 >= 0) && (cell[hod[1]-1][hod[0]-1] % 10 == 6) && (cell[hod[1] - 1][hod[0] - 1] / 10 != turn + 1)) return 1;
+	if ((hod[0] - 1 >= 0) && (cell[hod[1]][hod[0]-1] % 10 == 6) && (cell[hod[1]][hod[0] - 1] / 10 != turn+1)) return 1;
+
+	if (turn == 0)//peska
+	{
+		if ((hod[1] + 1 < 8) && (hod[0] + 1 < 8) && (cell[hod[1] + 1][hod[0] + 1] % 10 == 1) && (cell[hod[1] + 1][hod[0] + 1] / 10 == 2)) return 1;
+		if ((hod[1] + 1 < 8) && (hod[0] - 1 >= 0) && (cell[hod[1] + 1][hod[0] - 1] % 10 == 1) && (cell[hod[1] + 1][hod[0] - 1] / 10 == 2)) return 1;
+	}
+	else
+	{
+		if ((hod[1] - 1 >= 0) && (hod[0] + 1 < 8) && (cell[hod[1] - 1][hod[0] + 1] % 10 == 1) && (cell[hod[1] - 1][hod[0] + 1] / 10 == 1)) return 1;
+		if ((hod[1] - 1 >= 0) && (hod[0] - 1 >= 0) && (cell[hod[1] - 1][hod[0] - 1] % 10 == 1) && (cell[hod[1] - 1][hod[0] - 1] / 10 == 1)) return 1;
+	}
 	return 0;
 }
 int main()
 {
-
+	start_cell();
+	cout << chek_cell("b2") << endl; cout << chek_cell("b3") << endl; cout << chek_cell("b6") << endl; cout << chek_cell("h6") << endl; cout << chek_cell("e8") << endl;
 	return 0;
 }
